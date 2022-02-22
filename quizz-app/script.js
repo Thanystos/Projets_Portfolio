9540db8b406256107491cee89c42eed8df1174a9
Thanystos <guichardanthony@live.fr>
2022-02-22 20:35:07 +0100

    Beginning quizz app and listing questions

---
+++ quizz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'Quel titan de la mythologie grecque est condamné à soutenir le ciel ?',
+        a: 'Prométhée',
+        b: 'Cronos',
+        c: 'Atlas',
+        d: 'Rhéa',
+        correct: 'c'
+    },
+    {
+        question: 'Quel animal est l\'emblème des États-Unis ?',
+        a: 'L\'aigle royal',
+        b: 'Le vautour fauve',
+        c: 'Le pygargue',
+        d: 'Le milan noir',
+        correct: 'c'
+    },
+    {
+        question: 'Qui est le dieu soleil dans la mythologie égyptienne ?',
+        a: 'Osiris',
+        b: 'Rê',
+        c: 'Horus',
+        d: 'Anubis',
+        correct: 'b'
+    },
+    {
+        question: 'Quelle théorie doit-on à Isaac Newton ?',
+        a: 'La théorie des cordes',
+        b: 'La théorie de l\'évolution des espèces',
+        c: 'La théorie atomique',
+        d: 'La théorie de la gravitation universelle',
+        correct: 'd'
+    },
+    {
+        question: 'En quelle année Charlemagne se fait-il sacrer empereur ?',
+        a: '756 ap.J.-C',
+        b: '800 ap.J.-C',
+        c: '787 ap.J.-C',
+        d: '843 ap.J.-C',
+        correct: 'b'
+    }
+]
