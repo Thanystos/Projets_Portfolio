3f9b225816c5a98ba82f5941be5d703d5823608f
Thanystos <guichardanthony@live.fr>
2022-03-02 01:10:52 +0100

    End of project. We can now use the Local storage

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,10 +1,19 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+// Au chargement de la page, on vérifie le contenu du LS et on l'affiche si on trouve quelque chose
+if(notes) {
+    notes.forEach(note => {
+        addNewNote(note);
+    })
+}
+
 addBtn.addEventListener('click', () => {
     addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text = '') {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -14,9 +23,8 @@ function addNewNote() {
                 <button class="edit"><i class="fas fa-edit"></i></button>
                 <button class="delete"><i class="fas fa-trash-alt"></i></button>
             </div>
-            <div class="main hidden">
-            </div>
-            <textarea></textarea>
+            <div class="main ${text ? '' : 'hidden'}"></div>
+            <textarea class="${text ? 'hidden' : ''}"></textarea>
         </div>
     `;
 
@@ -26,6 +34,10 @@ function addNewNote() {
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
 
+    // Ces 2 lignes n'ont de sens que si on a du contenu dans le LS. On le copie dans textarea et on permet l'utilisation de marker
+    textArea.value = text;
+    main.innerHTML = marked.parse(text);
+
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden');
         textArea.classList.toggle('hidden');
@@ -33,14 +45,34 @@ function addNewNote() {
 
     deleteBtn.addEventListener('click', () => {
         note.remove();
+        updateLS();
     });
 
     textArea.addEventListener('input', (e) => {
         const { value } = e.target;
 
         main.innerHTML = marked.parse(value);
+
+        updateLS();
     });
 
     document.body.appendChild(note);
 }
+
+function updateLS() {
+    // Je récupère le texte de toutes les notes
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    // Pour chacunes des notes, je les stocke dans le tableau notes
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    // Que j'enregistre dans le LS
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+
 
