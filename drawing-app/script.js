c59f8120c6068552ee159c660220505b456ff3ae
Thanystos <guichardanthony@live.fr>
2022-03-05 13:48:40 +0100

    Can now draw line rather than circles. We can also erase the canvas content. End of project

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -3,27 +3,41 @@ const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEl = document.getElementById('size');
 const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 30;
 let isPressed = false;
 let color = 'black';
+let x = undefined;
+let y = undefined;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
     isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
 canvas.addEventListener('mouseup', () => {
     isPressed = false;
+    
+    x = undefined;
+    y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
     // On va récupérer la position de la souris lors de l'évent et demander à tracer un cercle à cet endroit
     if(isPressed) {
-        const x = e.offsetX;
-        const y = e.offsetY;
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x, y);
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        // on oublie pas d'actualiser x et y pour éviter de tracer une ligne par rapport à l'endroit où on avait cliqué la première fois
+        x = x2;
+        y = y2;
     }
 });
 
@@ -32,6 +46,15 @@ function drawCircle(x, y) {
     ctx.arc(x, y, size, 0, Math.PI * 2);  // On va tracer un cercle (voir méthode associée)
     ctx.fillStyle = color;
     ctx.fill(); // On dessine ce que'on a défini
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
 }
 
 increaseBtn.addEventListener('click', () => {
@@ -56,14 +79,18 @@ colorEl.addEventListener('change', (e) => {
     color = e.target.value;
 });
 
+clearEl.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+});
+
 function updateSizeOnScreen() {
     sizeEl.innerText = size;
 }
 
-function draw() {
-    /*ctx.clearRect(0, 0, canvas.clientWidth, canvas.height); // Efface le contenu du canvas
-    drawCircle(x++, y); // Trace le cercle en le décalant à chaque nouvel appel de la fonction
-    requestAnimationFrame(draw); // Entraîne la répétition de la fonction ce qui une sorte d'animation*/
-}
 
-draw();
+// Animation inutile pour notre projet...
+/*function draw() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height); // Efface le contenu du canvas
+    drawCircle(x++, y); // Trace le cercle en le décalant à chaque nouvel appel de la fonction
+    requestAnimationFrame(draw); // Entraîne la répétition de la fonction ce qui une sorte d'animation
+}*/
