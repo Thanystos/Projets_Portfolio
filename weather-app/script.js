6afb0ef9bcef09ea51be74a2e5c3d52c31dfac8e
Thanystos <guichardanthony@live.fr>
2022-03-06 15:08:05 +0100

    Using the openweathermap API.  First tests.

---
+++ weather-app/script.js
@@ -0,0 +1,14 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+
+    console.log(respData);
+
+}
+
+getWeatherByLocation('London');
